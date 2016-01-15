e0f286b061c57f22a399f209e36fff68aca5f688
lwasser <lwasser@neoninc.org>
2016-01-15 11:54:23 -0700

    all of the new R code

--- 01-shapefile-attributes.R
+++ 01-shapefile-attributes.R
@@ -1,94 +1,276 @@
-## ----load-libraries-data-------------------------------------------------
+## ----load-packages-data--------------------------------------------------
+#load packages
+#rgdal: for vector work; sp package should always load with rgdal. 
+library(rgdal)  
+#raster: for metadata/attributes- vectors or rasters
+library (raster)   
 
-library(rgdal)
-library (raster)   #notice sp package should always load with raster. 
-#in case the libraries and data are not still loaded
+#set working directory to data folder
+#setwd("pathToDirHere")
 
 #Import a polygon shapefile 
-aoiBoundary <- readOGR("boundaryFiles/HARV/", "HarClip_UTMZ18")
+aoiBoundary_HARV <- readOGR("NEON-DS-Site-Layout-Files/HARV/",
+                            "HarClip_UTMZ18")
 
 #Import a line shapefile
-lines <- readOGR( "boundaryFiles/HARV/",layer = "HARV_roads")
+lines_HARV <- readOGR( "NEON-DS-Site-Layout-Files/HARV/", "HARV_roads")
 
 #Import a point shapefile 
-point <- readOGR("boundaryFiles/HARV/", "HARVtower_UTM18N")
+point_HARV <- readOGR("NEON-DS-Site-Layout-Files/HARV/",
+                      "HARVtower_UTM18N")
 
 
 ## ----view-shapefile-metadata---------------------------------------------
-
 #view class
-class(x = aoiBoundary)
-class(x = lines)
-class(x = point)
+class(x = point_HARV)
 
+# x= isn't actually needed; it just specifies which object
 #view features count
-length(x = aoiBoundary)
-length(x = lines)
-length(x = point)
+length(point_HARV)
 
 #view crs - note - this only works with the raster package loaded
-crs(x = aoiBoundary)
-crs(x = lines)
-crs(x = point)
+crs(point_HARV)
 
-#view extent
-extent(x = squarePlot)
-extent(x = lines)
-extent(x = point)
+#view extent- note - this only works with the raster package loaded
+extent(point_HARV)
+
+#view metadata summary
+point_HARV
+
+## ----shapefile-attributes------------------------------------------------
+#just view the attributes & first 6attribute values of the data
+head(lines_HARV@data)
+
+#how many attributes are in our data?
+length(lines_HARV@data)
 
 
 ## ----view-shapefile-attributes-------------------------------------------
-
 #view just the attribute names for the lines spatial object
-names(lines@data)
+names(lines_HARV@data)
 
-#view all attribute data for the lines spatial object
-lines@data 
 
-#view just the tope 5 attributes for the lines spatial object
-head(lines@data)
+## ----challenge-code-attributes-classes, results="hide", echo=FALSE-------
+#1
+length(names(point_HARV@data))  #14 attributes
+names(aoiBoundary_HARV@data)  #1 attribute
 
-#view attributes for the other spatial objects
-aoiBoundary@data
-point@data
+#2
+head(point_HARV@data)  #Harvard University, LTER
 
+#3
+point_HARV@data  # C Country
 
 ## ----explore-attribute-values--------------------------------------------
-
 #view all attributes in the lines shapefile within the TYPE field
-lines$TYPE
-
-#view unique attributes in the lines shapefiles within the Type field
-levels(lines$TYPE)
+lines_HARV$TYPE
 
 
-## ----Subsetting shapefiles-----------------------------------------------
-
+## ----Subsetting-shapefiles-----------------------------------------------
 #view all attributes in the TYPE column
-lines$TYPE
+lines_HARV$TYPE
 
-#select features that are of TYPE "footpath"
-lines[lines$TYPE == "footpath",]
+# select features that are of TYPE "footpath"
+# could put this code into other function to only have that function work on
+# "footpath" lines
+lines_HARV[lines_HARV$TYPE == "footpath",]
+
+#save an object with only footpath lines
+footpath_HARV<-lines_HARV[lines_HARV$TYPE == "footpath",]
+footpath_HARV
+#how many features in our new object
+length(footpath_HARV)
+
+#plot just footpaths
+plot(footpath_HARV,
+     lwd=6,
+     main="Footpaths at NEON Harvard Forest Field Site")
+
+## ----convert-to-factor---------------------------------------------------
+#view the original class of the TYPE column
+class(lines_HARV$TYPE)
+#view levels or categories - not that there are no categories yet in our data!
+#the attributes are just read as a list of character elements.
+levels(lines_HARV$TYPE)
+
+#Convert the TYPE attribute into a factor
+lines_HARV$TYPE <- as.factor(lines_HARV$TYPE)
+#the class is now a factor
+class(lines_HARV$TYPE)
+#view the levels or categories associated with TYPE (4 total)
+levels(lines_HARV$TYPE)
+
+#how many features are in each category or level?
+summary(lines_HARV$TYPE)
+
+#plot the lines data, apply a diff color to each category
+plot(lines_HARV, col=lines_HARV$TYPE,
+     lwd=3,
+     main="Roads at the NEON Harvard Forest Field Site")
+
+## ----adjust-line-width---------------------------------------------------
+#make all lines thicker
+plot(lines_HARV, col=lines_HARV$TYPE,
+     main="Roads at the NEON Harvard Forest Field Site",
+     lwd=6)
+
+levels(lines_HARV$TYPE)
+#adjust width of each level
+#in this case, boardwalk (the first level) is the widest.
+plot(lines_HARV, col=lines_HARV$TYPE,
+     main="Roads at the NEON Harvard Forest Field Site \n Boardwalk Line Width Wider",
+     lwd=c(10,2,3,4))
 
 
-## ----Color lines by attribute--------------------------------------------
-#add legend to previous plot
+## ----add-legend-to-plot--------------------------------------------------
+plot(lines_HARV, col=lines_HARV$TYPE,
+     main="Roads at the NEON Harvard Forest Field Site\n Default Legend")
+#add a legend to our map
+legend("bottomright", NULL, levels(lines_HARV$TYPE), fill=palette())
 
 
-#color lines by TYPE attribute
-#1st create object that is the TYPE attribute (you could just do this directly
-#in the col {} below but this makes for cleaner code)
-type <- lines@data$TYPE
-#Bonus: if you prefer using slot(). 
-type <- slot(object = lines, name = "data")$TYPE
+## ----modify-legend-plot--------------------------------------------------
+plot(lines_HARV, col=lines_HARV$TYPE,
+     main="Roads at the NEON Harvard Forest Field Site \n Modified Legend")
+#add a legend to our map
+legend("bottomright", levels(lines_HARV$TYPE), fill=palette(), bty="n", cex=.8)
 
-#2nd set the color for all the lines 
-col <- rep(x= "black", length = length(type))
 
-#3rd specify the color you want for the TYPE level you want 
-col[type == "footpath"] <- "red"
+## ----adjust-palette-colors-----------------------------------------------
+#view default colors
+palette()
 
-#4th plot. Add is still true because we want this to map onto the previous plot 
-plot(x=lines, col=col,add=TRUE)
+#manually set the colors for the plot!
+palette(c("springgreen", "blue", "magenta", "red") )
+palette()
+
+#plot using new colors
+plot(lines_HARV, col=lines_HARV$TYPE,
+     main="Roads at the NEON Harvard Forest Field Site \n Pretty Colors")
+#add a legend to our map
+legend("bottomright", levels(lines_HARV$TYPE), fill=palette(), bty="n", cex=.8)
+
+
+## ----bicycle-map, echo=FALSE---------------------------------------------
+#view levels 
+levels(lines_HARV$BicyclesHo)
+
+#convert to factor if necessary
+lines_HARV$BicyclesHo <- as.factor(lines_HARV$BicyclesHo)
+levels(lines_HARV$BicyclesHo)
+#remove NA values
+lines_removeNA <- lines_HARV[na.omit(lines_HARV$BicyclesHo),]
+#set colors so only the allowed roads are magenta
+palette(c("magenta","grey","grey"))
+palette()
+#plot using new colors
+plot(lines_HARV, col=lines_HARV$BicyclesHo,
+     lwd=4,
+     main="Roads Where Bikes and Horses Are allowed  \n NEON Harvard Forest Field Site")
+#add a legend to our map
+legend("bottomright", 
+       levels(lines_HARV$BicyclesHo), 
+       fill=palette(), 
+       bty="n", cex=.8)
+
+
+## ----challenge-answer, echo=FALSE----------------------------------------
+
+#Plot multiple shapefiles
+plot(aoiBoundary_HARV, col = "grey93", border="grey",
+     main="NEON Harvard Forest\nField Site")
+plot(lines_HARV, 
+     col=lines_HARV$TYPE,
+     add = TRUE)
+plot(point_HARV, add  = TRUE, pch = 19, col = "purple")
+
+#assign plot to an object for easy modification!
+plot_HARV<- recordPlot()
+
+## ----customize-legend----------------------------------------------------
+
+#create a list of all labels
+labels <- c("Tower", "AOI", levels(lines_HARV$TYPE))
+labels
+
+#create a list of colors to use 
+plotColors <- c("purple", "grey",fill=palette())
+plotColors
+
+#create a list of pch values
+#these are the symbols that will be used for each legend value
+# ?pch will provide more information on values
+plotSym <- c(16,15,15,15,15,15)
+plotSym
+
+#Plot multiple shapefiles
+plot_HARV
+
+#to create a custom legend, we need to fake it
+legend("bottomright", 
+       legend=labels,
+       pch=plotSym, 
+       bty="n", 
+       col=plotColors,
+       cex=.8)
+
+
+## ----refine-legend-------------------------------------------------------
+#Create line object
+lineLegend = c(NA,NA,1,1,1,1)
+lineLegend
+plotSym <- c(16,15,NA,NA,NA,NA)
+plotSym
+
+#Plot multiple shapefiles
+plot_HARV
+
+#to create a custom legend, we need to fake it
+legend("bottomright", 
+       legend=labels, 
+       lty = lineLegend,
+       pch=plotSym, 
+       bty="n", 
+       col=plotColors,
+       cex=.9)
+
+
+## ----challenge-code-plot-other, results="hide", echo=FALSE---------------
+
+#Read the .csv file
+State.Boundary.US <- readOGR("NEON-DS-Site-Layout-Files/US-Boundary-Layers",
+          "US-State-Boundaries-Census-2014")
+
+palette(terrain.colors((50)))
+palette()
+
+plot(State.Boundary.US,
+     col=State.Boundary.US$NAME,
+     main="Roads at the NEON Harvard Forest Field Site \n 50 Colors")
+
+### part 2
+#open plot locations
+
+plotLocations <- readOGR("NEON-DS-Site-Layout-Files/HARV",
+          "PlotLocations_HARV")
+
+#how many unique soils?
+unique(plotLocations$soilTypeOr)
+
+#create new color palette
+palette(topo.colors((2)))
+palette()
+
+
+plot(plotLocations,
+     col=plotLocations$soilTypeOr, pch=18,
+     main="NEON Field Sites by Soil Type")
+#create legend 
+legend("bottomright", 
+       legend=c("Intceptisols","Histosols"),
+       pch=18, 
+       col=palette(),
+       bty="n", 
+       cex=1)
 
 
---
+++ 04-vector-raster-integration-advanced.R
@@ -0,0 +1,232 @@
+## ----Plot vector-raster overlay, echo=FALSE------------------------------
+library(rgdal)  #for vector work; sp package should always load with rgdal. 
+library (raster)
+
+#set working directory to data folder
+#setwd("pathToDirHere")
+
+
+## ----view-extents, echo=FALSE, results='hide'----------------------------
+
+#Import a polygon shapefile 
+aoiBoundary_HARV <- readOGR("NEON-DS-Site-Layout-Files/HARV/",
+                            "HarClip_UTMZ18")
+
+#Import a line shapefile
+lines_HARV <- readOGR( "NEON-DS-Site-Layout-Files/HARV/", 
+                       "HARV_roads")
+
+#Import a point shapefile 
+point_HARV <- readOGR("NEON-DS-Site-Layout-Files/HARV/",
+                      "HARVtower_UTM18N")
+
+chm_HARV <- raster("NEON_RemoteSensing/HARV/CHM/HARV_chmCrop.tif")
+
+utm18nCRS <- crs(point_HARV)
+
+#Read the .csv file
+plot.locations_HARV <- 
+  read.csv("NEON-DS-Site-Layout-Files/HARV/HARV_PlotLocations.csv",
+           stringsAsFactors = FALSE)
+
+#note that the easting and northing columns are in columns 1 and 2
+plot.locationsSp_HARV <- SpatialPointsDataFrame(plot.locations_HARV[,1:2],
+                    plot.locations_HARV,    #the R object to convert
+                    proj4string = utm18nCRS)   # assign a CRS 
+
+plot(extent(lines_HARV),
+     col="purple", lwd="3",
+     xlab="Easting", ylab="Northing",
+    main="Extent Boundary of Several Spatial Files")
+plot(extent(plot.locationsSp_HARV), add=TRUE)
+plot(extent(aoiBoundary_HARV),
+     add=TRUE,
+     col="blue", lwd=4)
+plot(extent(chm_HARV),
+     add=TRUE, lwd=5,
+     col="springgreen")
+
+legend("top", 
+        legend=c("Roads","Plot Locations","Tower AOI", "CHM"),
+       lwd=3,
+       col=c("purple","black","blue","springgreen"),
+       xpd = TRUE, 
+       horiz = TRUE, 
+       inset = c(0, 0), 
+       bty = "n",  
+       cex = .8)
+
+
+
+## ----reset-par, results="hide", echo=FALSE-------------------------------
+#reset par
+dev.off()
+
+## ----load-libraries-data-------------------------------------------------
+
+#set working directory to data folder
+#setwd("pathToDirHere")
+
+#Imported in L00: Vector Data in R - Open & Plot Data
+# shapefile 
+aoiBoundary_HARV <- readOGR("NEON-DS-Site-Layout-Files/HARV/",
+                            "HarClip_UTMZ18")
+#Import a line shapefile
+lines_HARV <- readOGR( "NEON-DS-Site-Layout-Files/HARV/",
+                       "HARV_roads")
+#Import a point shapefile 
+point_HARV <- readOGR("NEON-DS-Site-Layout-Files/HARV/",
+                      "HARVtower_UTM18N")
+
+#Imported in L02: .csv to Shapefile in R
+#import raster Canopy Height Model (CHM)
+chm_HARV <- 
+  raster("NEON_RemoteSensing/HARV/CHM/HARV_chmCrop.tif")
+
+
+## ----Crop-by-vector-extent-----------------------------------------------
+#plot full CHM
+plot(chm_HARV,
+     main="LiDAR CHM - Not Cropped\nNEON Harvard Forest Field Site")
+
+#crop the chm
+chm_HARV_Crop <- crop(x = chm_HARV, y = aoiBoundary_HARV)
+
+#plot full CHM
+plot(extent(chm_HARV),
+     lwd=4,col="purple",
+     main="LiDAR CHM - Cropped\nNEON Harvard Forest Field Site",
+     xlab="easting", ylab="northing")
+plot(chm_HARV_Crop,
+     add=TRUE)
+
+
+## ----view-crop-extent----------------------------------------------------
+#view the data in a plot
+plot(aoiBoundary_HARV, lwd=8, border="blue",
+     main = "Cropped LiDAR CHM \n NEON Harvard Forest Field Site")
+
+plot(chm_HARV_Crop, add = TRUE)
+
+#lets look at the extent of all of our objects
+extent(chm_HARV)
+extent(chm_HARV_Crop)
+extent(aoiBoundary_HARV)
+
+
+## ----challenge-code-crop-raster-points, include=TRUE, results="hide", echo=FALSE----
+
+#Created/imported in L02: .csv to Shapefile in R
+plot.locationSp_HARV <- readOGR("NEON-DS-Site-Layout-Files/HARV/",
+                                "PlotLocations_HARV")
+
+#crop the chm 
+CHM_plots_HARVcrop <- crop(x = chm_HARV, y = plot.locationsSp_HARV)
+
+plot(CHM_plots_HARVcrop,
+     main="Study Plot Locations\n NEON Harvard Forest")
+plot(plot.locationSp_HARV, 
+     add=TRUE,
+     pch=19,
+     col="blue")
+
+
+## ----hidden-extent-chunk-------------------------------------------------
+#extent format (xmin,xmax,ymin,ymax)
+new.extent <- extent(732161.2, 732238.7, 4713249, 4713333)
+class(new.extent)
+
+## ----crop-using-drawn-extent---------------------------------------------
+
+#crop raster
+CHM_HARV_manualCrop <- crop(x = chm_HARV, y = new.extent)
+
+#plot extent boundary and newly cropped raster
+plot(aoiBoundary_HARV, 
+     main = "Manually Cropped Raster\n NEON Harvard Forest Field Site")
+plot(new.extent, col="blue", lwd=4,add = TRUE)
+plot(CHM_HARV_manualCrop, add = TRUE)
+
+
+## ----extract-from-raster-------------------------------------------------
+
+#extract tree height for AOI
+#set df=TRUE to return a data.frame rather than a list of values
+tree_height <- extract(x = chm_HARV, 
+                       y = aoiBoundary_HARV, 
+                       df=TRUE)
+
+#view the object
+head(tree_height)
+
+nrow(tree_height)
+
+## ----view-extract-histogram----------------------------------------------
+#view histogram of tree heights in study area
+hist(tree_height$HARV_chmCrop, 
+     main="Histogram of CHM Height Values (m) \nNEON Harvard Forest Field Site",
+     col="springgreen",
+     xlab="Tree Height", ylab="Frequency of Pixels")
+
+#view summary of values
+summary(tree_height$HARV_chmCrop)
+
+
+## ----summarize-extract---------------------------------------------------
+#extract the average tree height (calculated using the raster pixels)
+#located within the AOI polygon
+av_tree_height_AOI <- extract(x = chm_HARV, 
+                              y = aoiBoundary_HARV,
+                              fun=mean, df=TRUE)
+
+#view output
+av_tree_height_AOI
+
+
+## ----extract-point-to-buffer---------------------------------------------
+
+#what are the units of our buffer
+crs(point_HARV)
+
+#extract the average tree height (calculated using the raster pixels)
+#located within the AOI polygon
+#use a buffer of 20 meters and mean function (fun) 
+av_tree_height_tower <- extract(x = chm_HARV, 
+                               y = point_HARV, 
+                               buffer=20,
+                               fun=mean, 
+                               df=TRUE)
+
+#view data
+head(av_tree_height_tower)
+
+#how many pixels were extracted
+nrow(av_tree_height_tower)
+
+
+
+## ----challenge-code-extract-plot-tHeight, include=TRUE, results="hide", echo=FALSE----
+
+#first import the plot location file.
+
+#Import a polygon shapefile 
+plot.locations_HARV <- readOGR("NEON-DS-Site-Layout-Files/HARV/",
+                            "PlotLocations_HARV")
+
+#extract data at each plot location
+meanTreeHt_plots_HARV <- extract(x = chm_HARV, 
+                               y = plot.locations_HARV, 
+                               buffer=20,
+                               fun=mean, 
+                               df=TRUE)
+
+#view data
+meanTreeHt_plots_HARV
+
+#plot data
+plot(meanTreeHt_plots_HARV,
+     main="Tree Height by Plot\nNEON Harvard Forest Field Site",
+     xlab="Plot ID", ylab="Tree Height (m)",
+     pch=16)
+
+
