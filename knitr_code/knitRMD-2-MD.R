f1641f18e622ff8e5810405df669223d098fa6d5
Leah Wasser <lwasser@NEON-04193.local>
2016-02-09 10:08:41 -0700

    updated knitr to place figs in subdir in Rfigs

--- knitr_code/knitRMD-2-MD.R
+++ knitr_code/knitRMD-2-MD.R
@@ -29,7 +29,7 @@ setwd(wd)
 postsDir <- ("_posts/R/dc-spatial-vector/")
 
 #images path
-imagePath <- "images/rfigs/"
+imagePath <- "images/rfigs/dc-spatial-vector/"
 
 #set the base url for images and links in the md file
 base.url="{{ site.baseurl }}/"
@@ -45,6 +45,7 @@ if (file.exists(paste0(wd,"/","images"))){
   #create image directory structure
   dir.create(file.path(wd, "images/"))
   dir.create(file.path(wd, "images/rfigs"))
+  dir.create(file.path(wd, "images/rfigs/dc-spatial-vector/"))
   dir.create(file.path(wd, figDir))
   print("image directories created!")
 }
@@ -84,6 +85,16 @@ for (files in rmd.files) {
   
   #COPY image director, rmd file OVER to the GIT SITE###
   
+  #make sure image subdir exists
+  #note this will fail if the sub dir doesn't exist
+  if (file.exists(paste0(gitRepoPath,"images/rfigs/dc-spatial-vector/"))){
+    print("image dir exists - all good")
+  } else {
+    #create image directory structure
+    dir.create(file.path(gitRepoPath, "images/rfigs"))
+    dir.create(file.path(gitRepoPath, "images/rfigs/dc-spatial-vector/"))
+    print("git image directories created!")
+  }
   #copy image directory over
   file.copy(paste0(wd,"/",fig.path), paste0(gitRepoPath,imagePath), recursive=TRUE)
   
